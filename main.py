575c53dfd6552247ad889605eda697b630b4f150
summerlightningman <div8@bk.ru>
2020-03-10 14:58:10 +0300

    First commit

---
+++ main.py
@@ -0,0 +1,18 @@
+from PyQt5 import QtWidgets
+from window import Ui_MainWindow
+
+
+class Browser(QtWidgets.QWidget, Ui_MainWindow):
+    def __init__(self, parent=None):
+        QtWidgets.QWidget.__init__(self, parent)
+        self.setupUi(self)
+
+
+if __name__ == '__main__':
+    import sys
+
+    app = QtWidgets.QApplication(sys.argv)
+    browser = Browser()
+    browser.setWindowTitle('Бряузер гугол родий')
+    browser.show()
+    sys.exit(app.exec_())
